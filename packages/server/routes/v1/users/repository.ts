7137b96af057110eb8cc1380d670eb5ec7bc5991
shahroq <shahroq@yahoo.com>
2025-12-01 12:48:44 +0330

    remove users for now

--- packages/server/routes/v1/users/repository.ts
+++
@@ -1,8 +0,0 @@
-import { BaseRepository } from "@/common/repository/base.repository";
-import type { User } from "./types";
-
-export class UserRepository extends BaseRepository<User> {
-  constructor() {
-    super("users");
-  }
-}
