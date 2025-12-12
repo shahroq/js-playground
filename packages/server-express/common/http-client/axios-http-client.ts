bbfea74f8ec5c408a5bb038e37ac8ff9964a8998
shahroq <shahroq@yahoo.com>
2025-12-12 14:46:05 +0330

    refactor http-clients: no need for return type declarations on implementations

--- packages/server-express/common/http-client/axios-http-client.ts
+++ packages/server-express/common/http-client/axios-http-client.ts
@@ -16,16 +16,12 @@ export class AxiosHttpClient implements IHttpClient {
     return this.httpClient as T;
   }
 
-  async get<T = any>(url: string, config?: RequestConfig): Promise<T> {
+  async get<T = any>(url: string, config?: RequestConfig) {
     const response = await this.httpClient.get<T>(url, config);
     return response.data;
   }
 
-  async post<T = any>(
-    url: string,
-    data?: any,
-    config?: RequestConfig
-  ): Promise<T> {
+  async post<T = any>(url: string, data?: any, config?: RequestConfig) {
     const response = await this.httpClient.post<T>(url, data, config);
     return response.data;
   }
