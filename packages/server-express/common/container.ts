00afc556227a3d252410314183e276c28a187ab6
shahroq <shahroq@yahoo.com>
2025-12-12 16:54:57 +0330

    rename: app-envelope

--- packages/server-express/common/container.ts
+++ packages/server-express/common/container.ts
@@ -1,7 +1,6 @@
-import { queryOptions } from "../routes/v1/reviews/query.options";
 // common
 import config from "./config";
-import { getAppEnvelope } from "./envelope/factory";
+import { getAppEnvelope } from "./app-envelope/factory";
 import { getValidatorHandler } from "./validation/factory";
 import { getDBAdapter } from "./db-adapter/factory";
 import { getHttpClient } from "./http-client/factory";
@@ -27,8 +26,8 @@ export { default as AppError, isAppError } from "./app-error/app-error";
 export * from "@/common/meta-data/meta-data";
 export * from "@/common/app-error/global-error.middleware";
 export * from "@/common/middlewares/undefined-routes.middleware";
-export * from "@/common/envelope/attach-system-data.middleware";
-export * from "@/common/envelope/envelop-response.middleware";
+export * from "@/common/app-envelope/attach-system-data.middleware";
+export * from "@/common/app-envelope/envelop-response.middleware";
 export * from "@/common/app-query/normalize-query.middleware";
 export * from "@/common/app-query/app-query";
 export { queryOptions as defaultQueryOptions } from "@/common/app-query/default.options";
