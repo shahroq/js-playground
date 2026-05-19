d18982e8372d67d539e0709c599cd5046b7c710d
shahroq <shahroq@yahoo.com>
2026-05-19 19:10:57 +0330

    nextjs: tasks w-prisma

--- packages/client-nextjs-16/app/tasks/w-prisma/create/page.tsx
+++ packages/client-nextjs-16/app/tasks/w-prisma/create/page.tsx
@@ -1,17 +1,21 @@
-import { Hero } from "@gpublic/comps";
+import { Button, FormControl } from "@gpublic/comps";
 import { PageTitle } from "@/comps/PageTitle";
 import { Page } from "@/gpublic/types/types";
 
 const page: Page = {
   title: "Tasks",
-  breadcrumb: [{ label: "List", path: "/tasks/w-prisma" }, { label: "Create" }],
+  breadcrumb: [
+    { label: "Tasks" },
+    { label: "List", path: "/tasks/w-prisma" },
+    { label: "Create" },
+  ],
 };
 
 export default function New() {
   return (
     <section>
       <PageTitle page={page} />
-      <Hero title="New Task" />
+      <form className="form"></form>
     </section>
   );
 }
