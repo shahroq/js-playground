264a2027a67e4cd8b190bd7f9137ce042c075c1b
shahroq <shahroq@yahoo.com>
2026-06-06 14:09:18 +0330

    add EntityId global type

--- gpublic/types/types.ts
+++ gpublic/types/types.ts
@@ -3,6 +3,8 @@ import type { PropsWithChildren } from "react";
 export type PropsWithCN = { className?: string };
 export type PropsWithChildrenAndCN = PropsWithChildren & PropsWithCN;
 
+export type EntityId = number | string;
+
 export type NavItem = {
   label: string;
   path?: string;
