66c1d9b51138c0e7279a2c5b80b5a9f428cdcb71
shahroq <shahroq@yahoo.com>
2026-06-10 13:49:19 +0330

    add cn using clsx

--- packages/shared/utils/cn.ts
+++ packages/shared/utils/cn.ts
@@ -1,3 +1,12 @@
-export function cn(...classes: Array<string | undefined | false | null>) {
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/*
+export function cnPlain(...classes: Array<string | undefined | false | null>) {
   return classes.filter(Boolean).join(" ");
+}
+*/
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
