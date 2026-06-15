1d1ae8421fd0dee878427d55203de30122312283
shahroq <shahroq@yahoo.com>
2026-06-15 14:43:36 +0330

    add some img to repo

--- packages/shared/public/img/index.ts
+++ packages/shared/public/img/index.ts
@@ -1,8 +1,6 @@
 export { default as avatar } from "./avatar.png";
-export { default as avatar1 } from "./avatar-1.png";
-export { default as avatar2 } from "./avatar-2.png";
-export { default as avatar3 } from "./avatar-3.png";
-export { default as avatar4 } from "./avatar-4.png";
+export { default as avatar1 } from "./users/avatar-01.png";
+export { default as avatar2 } from "./users/avatar-02.png";
 
 export { default as appleTouchIcon } from "./apple-touch-icon.png";
 export { default as favicon } from "./favicon.ico";
