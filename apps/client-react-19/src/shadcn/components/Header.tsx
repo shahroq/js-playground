fecd4f561565caac01715dde1100b32d7359d581
shahroq <shahroq@yahoo.com>
2026-06-15 15:02:39 +0330

    add children to Header comp

--- apps/client-react-19/src/shadcn/components/Header.tsx
+++ apps/client-react-19/src/shadcn/components/Header.tsx
@@ -1,3 +1,4 @@
+import type { PropsWithChildren } from "react";
 import type { NavItem, Page } from "@jsp/shared/types";
 import {
   Breadcrumb,
@@ -10,10 +11,10 @@ import {
 import { Separator } from "@/shadcn/components/ui/separator";
 import { SidebarTrigger } from "@/shadcn/components/ui/sidebar";
 
-type Props = { page: Page };
+type Props = PropsWithChildren<{ page: Page }>;
 type PropsAppBreadcrumb = { items?: NavItem[] };
 
-export function Header({ page }: Props) {
+export function Header({ page, children }: Props) {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -23,6 +24,7 @@ export function Header({ page }: Props) {
           className="mx-2 data-[orientation=vertical]:h-4"
         />
         {page.breadcrumb && <AppBreadcrumb items={page.breadcrumb} />}
+        {children}
       </div>
     </header>
   );
