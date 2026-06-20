2f1a136de7f4500623280e5285bb5060eeaeb3c9
shahroq <shahroq@yahoo.com>
2026-06-20 12:45:56 +0330

    next config: location of dev icon

--- apps/client-nextjs-16/next.config.ts
+++ apps/client-nextjs-16/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    position: "bottom-right",
+  },
   transpilePackages: ["@jsp/shared"],
   // productionBrowserSourceMaps: true, // useful even in prod debugging
 };
